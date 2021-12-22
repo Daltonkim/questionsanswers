007790881834c61cfde619da2071fba1277958c6
daltonkim <dalton.korir@incentro.com>
2021-12-23 00:20:39 +0300

    my reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { questions } from '../modules/home/redux/_reducer'
+import { modals } from '../modules/modals/_reducer'
 
 const rootReducer = combineReducers({
-    questions
+    questions,
+    modals
 });
 
 export default rootReducer;
