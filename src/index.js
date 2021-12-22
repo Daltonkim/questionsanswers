793c553b34d8aad2e53ba9fed508474f33f3fa37
daltonkim <dalton.korir@incentro.com>
2021-12-22 16:00:28 +0300

    react suspense

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,8 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Suspense fallback={("Loading")}>
+      <App />
+    </Suspense>
+  </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
