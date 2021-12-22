48cfaa849fd27893f3f7d07de8b9098717fe1316
daltonkim <dalton.korir@incentro.com>
2021-12-22 15:59:00 +0300

    connect redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import './assets/main.scss';
 import Home from './modules/home';
 
 function App() {
   return (
-    <div className="app">
-      <div className='app__column'>
-        <p>Here you can find 1 question. Feel free to create your own questions!</p>
+    <Provider store={store}>
+      <div className="app">
+        <div className='app__column'>
+          <p>Here you can find 1 question. Feel free to create your own questions!</p>
+        </div>
+        <div className='app__column'>
+          <h1>Question and Answer tool</h1>
+          <Home/>
+        </div>
       </div>
-      <div className='app__column'>
-        <h1>Question and Answer tool</h1>
-        <Home></Home>
-      </div>
-    </div>
+    </Provider>
   );
 }
 
