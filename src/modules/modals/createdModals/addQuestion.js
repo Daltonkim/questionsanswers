aa560d60c1169b636653a65620c637ea022d66b8
daltonkim <dalton.korir@incentro.com>
2021-12-30 12:29:48 +0300

    add and edit new field

--- src/modules/modals/createdModals/addQuestion.js
+++ src/modules/modals/createdModals/addQuestion.js
@@ -8,6 +8,7 @@ export const AddQuestion = ({ modalID }) => {
 
     const [question, setQuestion] = useState('')
     const [answer, setAnswer] = useState('')
+    const [hasDelay, setDelay] = useState(false)
     const [, setDisabled] = useState(true)
 
     const dispatch = useDispatch();
@@ -18,10 +19,12 @@ export const AddQuestion = ({ modalID }) => {
         } else {
             setDisabled(true)
         }
-        
-    }, [question, answer,])
+    }, [question, answer])
 
-    
+
+    const handleCheckChange = () => {
+        setDelay(!hasDelay)
+    }
 
     //add questions
     const addQuestion = () => {
@@ -29,6 +32,7 @@ export const AddQuestion = ({ modalID }) => {
             const obj = {
                 question: question,
                 answer: answer,
+                hasDelay: hasDelay,
                 uid: uuidv4()
             }
             dispatch(questionaireActions.addQuestion(obj));
@@ -47,12 +51,16 @@ export const AddQuestion = ({ modalID }) => {
             <div className="select-file p-10">
                 <form onSubmit={() => addQuestion()}>
                     <label htmlFor="question">Question</label>
-                    <input required  data-testid="question" value={question} className="app__question-qtext" onChange={(e) => setQuestion(e.target.value)} type="text" name="question"></input>
+                    <input required data-testid="question" value={question} className="app__question-qtext" onChange={(e) => setQuestion(e.target.value)} type="text" name="question"></input>
                     <label htmlFor="answer">Answer</label>
                     <textarea data-testid="answer" required value={answer} name="answer" rows="6" onChange={(e) => setAnswer(e.target.value)} className="app__question-qtextarea"></textarea>
+                    <label htmlFor='hasDelay' className="checkbox">
+                        <input type="checkbox" value={hasDelay} name="hasDelay" className="app__question-delay" onChange={(e) => handleCheckChange(e.target.value)}></input>
+                        <span>Add a 5 second delay</span>
+                    </label>
                     <div className="btn-group-actions site-modal__actions">
-                        <button  type='submit'  data-testid="submit-button" className="app__question-actions-create" >Add Question</button>
-                        <button  className="danger" onClick={() => closeModal()}>Close</button>
+                        <button type='submit' data-testid="submit-button" className="app__question-actions-create" >Add Question</button>
+                        <button className="danger" onClick={() => closeModal()}>Close</button>
                     </div>
                 </form>
             </div>
